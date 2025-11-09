dec9179175600e82dd6807d850f2385f3ee4f422
jelsikgvpt-a11y <jelsik.gvpt@gmail.com>
2025-11-09 17:29:29 +0100

    du3

---
+++ du3.py
@@ -0,0 +1,7 @@
+#Máte string: "Učím sa Java. Java je záživná."
+#Nahraďte všetky výskyty "Java" slovom "Python".
+#Vypíšte pôvodný aj nový text.
+text = "Učím sa Java. Java je záživná."
+novy_text = text.replace("Java", "Python")  
+print("Pôvodný text:", text)
+print("Nový text:", novy_text)
